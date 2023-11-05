e4fd7e94aa5db10652fddcd5e143dd4d89d2119b
dan-watkins <39423254+dan-watkins@users.noreply.github.com>
2023-11-05 12:58:23 -0600

    got 6 cards for projects, and projects are saving on initial load, but not persisting for some reason

--- react-portfolio/src/pages/ProjectPage.jsx
+++ react-portfolio/src/pages/ProjectPage.jsx
@@ -3,21 +3,27 @@ import CardContainer from "../components/UI/Card";
 import API from "../utils/API";
 
 export default function ProjectPage() {
-  const [project, setProject] = useState([]);
-
-  const getProject = async () => {
-    const data = await API.getAllProjects();
-    setProject(data);
-    console.log(project);
-  };
+  const [project, setProject] = useState({});
 
   useEffect(() => {
+    const getProject = async () => {
+      const data = await API.getAllProjects();
+      const list = data.data;
+      setProject(list);
+      console.log(project);
+    };
+
     getProject();
   }, []);
 
   return (
     <>
-      <CardContainer project={project}></CardContainer>
+      <CardContainer project={project[0]}></CardContainer>
+      <CardContainer project={project[1]}></CardContainer>
+      <CardContainer project={project[2]}></CardContainer>
+      <CardContainer project={project[3]}></CardContainer>
+      <CardContainer project={project[4]}></CardContainer>
+      <CardContainer project={project[5]}></CardContainer>
     </>
   );
 }
